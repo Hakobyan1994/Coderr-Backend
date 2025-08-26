b5400acba09c18542acefb675955505f716f4af2
Hakobyan1994 <mesrop.hakobyan@web.de>
2025-08-27 00:12:00 +0200

    added css in Admin

--- Coderr/urls.py
+++ Coderr/urls.py
@@ -18,13 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth',include('rest_framework.urls')),
     path('api/',include('api_urls.urls'))
-]
+] + staticfiles_urlpatterns()
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
